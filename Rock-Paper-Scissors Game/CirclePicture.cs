7faba2f4d3218fc553a8c0359dac2204f897d404
BungPeerapat <mashiro74122541@gmail.com>
2020-03-11 21:31:12 +0700

    Add CircularPicture

---
+++ Rock-Paper-Scissors Game/CirclePicture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors_Game
+{
+    public class CircularButton : CB1
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath g = new GraphicsPath();
+            g.AddEllipse(0, 0 ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(g);
+            base.OnPaint(pe);
+        }
+    }
+}
---
+++ Rock-Paper-Scissors Game/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors_Game
+{
+    public class CircularPictureBox : CB1
+    {
+        protected override voide OnPaint(PaintEventAge pe)
+        {
+            GraphicsPath g = new GraphicsPath();
+            g.AddEllipse(0, 0 ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(g);
+            base.OnPaint(pe);
+        }
+    }
+}
